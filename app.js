53c175f55455776d311170e4844b4c30b5703ba7
caodav17 <34912165+caodav17@users.noreply.github.com>
2018-09-13 02:29:48 -0700

    error validation
    checks if input is correct

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const multer = require("multer");
 const exec = require("child_process").exec;
+const session = require("express-session");
+const validator = require("express-validator");
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public/images");
     },
     filename: function (req, file, cb) {
-        cb(null, "pixelate.jpg");
+        const imgPath = req.session.id + "_preview.jpg";
+        cb(null, imgPath);
     }
 });
 const upload = multer({
@@ -19,29 +23,58 @@ const upload = multer({
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set("views", "./views");
-app.set("view engine", "pug");
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", "./views")
+    .set("view engine", "pug")
+    .use(express.static(path.join(__dirname, "/public")))
+    .use(bodyParser.urlencoded({
+        extended: false
+    }))
+    .use(session({
+        secret: "secret-key",
+        resave: true,
+        saveUninitialized: true
+    }))
+    .use(validator());
+
 
 app.get("/", function (req, res) {
-        res.render("index");
+        console.log(req.session.errors);
+        res.render("index", {
+            session_id: req.session.id,
+            errors: req.session.errors
+        });
+        req.session.errors = null;
+        console.log("cleared");
+        console.log(req.session.errors);
     })
     .post("/upload", upload.single("image"), function (req, res) {
+        req.session.errors = null;
         res.redirect("/");
     })
-    .post("/send", function (req, res) {
+    .post("/download", function (req, res) {
         const size = req.body.size;
-        const toExec = "java -jar ./public/pixelate.jar " + size;
-        const child = exec(toExec,
-            function (error, stdout, stderr) {
-                if (error !== null) {
-                    console.log("Error -> " + error);
-                }
-                res.download(path.join(__dirname, "/public/images/" + size + "x" + size + ".jpg"));
-            });
+        req.checkBody("size", "Input a number").isInt();
+        const errors = req.validationErrors();
+        req.session.errors = errors;
+        if (errors) {
+            res.redirect("/");
+        } else {
+            const toExec = "java -jar ./public/pixelate.jar " + size + " " + req.session.id;
+            const child = exec(toExec,
+                function (error, stdout, stderr) {
+                    if (error !== null) {
+                        const fakeError = "error";
+                        req.checkBody("fakeError", "Upload an image").isInt();
+                        req.session.errors = req.validationErrors();
+                        res.redirect("/");
+                        console.log("Error -> " + error);
+                    } else {
+                        res.download(path.join(__dirname, "/public/images/" + req.session.id + "_" + size + "x" + size + ".jpg"));
+                        res.redirect("/");
+                    }
+                });
+        }
+        
     });
 
 app.listen(port);
